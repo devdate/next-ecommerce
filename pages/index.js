c661fd45b726887b3074ef71e845c5a881c668c0
devdate <tariqanwar5897@gmail.com>
2022-11-14 02:11:03 +0530

    fix

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home({ productsData }) {
   );
 }
 export const getServerSideProps = async () => {
-  const res = await axios.get("http://localhost:3000/api/products");
+  const res = await axios.get(`${process.env.PUBLIC_URL}/api/products`);
   return {
     props: {
       productsData: res.data,
